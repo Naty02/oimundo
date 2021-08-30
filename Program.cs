859f6a0b382d6fe77bb8a6d32fcd7f13ef13bac3
Natalie Gonçalves <nataliegoncalvesdasilva02@gmail.com>
2021-08-29 22:52:27 -0300

    resolução do exercício oimundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oimundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("oi mundo, meu nome é Natalie!");
+        }
+    }
+}
